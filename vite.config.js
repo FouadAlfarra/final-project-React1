28be8e31c17d3746e49e2f3ed902b8f82e5fe6b4
Fouad ALFARRA <fouadalfarra44@gmail.com>
2026-07-27 19:56:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/final-project-React1",
+    base: "/react-final-lab",
   plugins: [react()],
 })
